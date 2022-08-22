d70f4026fce1c93785e395ea0c77417a678364b4
Madam Ella <madamellamalala@gmail.com>
2022-08-22 20:59:42 +0100

    .

--- assets/scripts/www-dot-chamchabranding-dot-corporomalala-dot-com.js
+++ assets/scripts/www-dot-chamchabranding-dot-corporomalala-dot-com.js
@@ -12,29 +12,30 @@ Written for -- www.chamchabranding.corporomalala.com
 /** WWW ***/
 
 	/*** GLOBAL VARIABLES & OBJECTS ***/
-	var ib_form = document.querySelector(".js-form");
+	var ib_form_message = document.querySelector(".js-form_message");
+	var ib_form_membership = document.querySelector(".js-form_membership");
 	/*** END GLOBAL VARIABLES & OBJECTS ***/
 	
 	/*** SETUP ***/
-//	animateCards();
 	/*** END SETUP ***/
 	
 	/*** EVENTS ***/
-	ib_form.addEventListener("submit", ib_handleSubmit);
+	ib_form_message.addEventListener("submit", ib_handleSubmit_message);
+	ib_form_membership.addEventListener("submit", ib_handleSubmit_membership);
 	/*** END EVENTS ***/
 	
 	/*** ACTIONS ***/
 	/*** END ACTIONS ***/
 	
 	/*** FUNCTIONS ***/
-	async function ib_handleSubmit(event) {
+	async function ib_handleSubmit_message(event) {
 		event.preventDefault();
 		
-		var status = document.querySelector(".js-form-status");
+		var status = document.querySelector(".js-form_message-status");
 		var data = new FormData(event.target);
 		
 		fetch(event.target.action, {
-			method: ib_form.method,
+			method: ib_form_message.method,
 			body: data,
 			headers: {
 				"Accept": "application/json"
@@ -42,18 +43,47 @@ Written for -- www.chamchabranding.corporomalala.com
 		}).then(response => {
 			if(response.ok) {
 				status.innerHTML = "Thanks for your submission!";
-				ib_form.reset();
+				ib_form_message.reset();
 			} else {
 				response.json().then(data => {
 					if(Object.hasOwn(data, "errors")) {
 						status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
 					} else {
-						status.innerHTML = "Oops! There was a problem submitting your ib_form.";
+						status.innerHTML = "Oops! There was a problem submitting your message_form.";
 					}
 				});
 			}
 		}).catch(error => {
-			status.innerHTML = "Oops! There was a problem submitting your ib_form.";
+			status.innerHTML = "Oops! There was a problem submitting your membership_form.";
+		});
+	}
+	async function ib_handleSubmit_membership(event) {
+		event.preventDefault();
+		
+		var status = document.querySelector(".js-form_membership-status");
+		var data = new FormData(event.target);
+		
+		fetch(event.target.action, {
+			method: ib_form_message.method,
+			body: data,
+			headers: {
+				"Accept": "application/json"
+			}
+		}).then(response => {
+			if(response.ok) {
+				status.innerHTML = "Thanks for your submission!";
+				ib_form_message.reset();
+			} else {
+				response.json().then(data => {
+					if(Object.hasOwn(data, "errors")) {
+						status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
+					} else {
+						status.innerHTML = "Oops! There was a problem submitting your membership_form.";
+					}
+				});
+			}
+		}).catch(error => {
+			status.innerHTML = "Oops! There was a problem submitting your membership_form.";
 		});
 	}
 	/*** END FUNCTIONS ***/
